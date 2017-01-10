920bfc2888af5313f931815235c7d11a283569cb
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-01-10 12:58:34 +0800

    edge_spec check the correct types

--- spec/edge_spec.rb
+++ spec/edge_spec.rb
@@ -14,9 +14,9 @@ describe Edge, '#initialize' do
       e2 = Edge.new(rows[4].clone, mm = 500)
       e3 = Edge.new(rows[4].clone, mm = 500, true)
 
-      expect(e.src).to eq '14454'
-      expect(e.length).to eq '38.6150017'
-      expect(e2.mm).to eq '0.003262'
-      expect(e3.mm).to eq '0.121001662'
+      expect(e.src).to eq 14454
+      expect(e.length).to eq 38.6150017
+      expect(e2.mm).to eq 0.003262
+      expect(e3.mm).to eq 0.121001662
     end
 end
