db2625819671e0bd9e8f0fcf42103934825d22ee
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-02-07 14:51:13 +0800

    more kinds of reading types

--- IO/reader.rb
+++ IO/reader.rb
@@ -19,9 +19,21 @@ class CSVReader
       end
     when 'Intersection'
       CSV.foreach(@file_path) do |row|
-        raw_lines << [row[0].to_i, row[2].to_f, row[3].to_f] if row[1] == '1'
+        raw_lines << get_nodes_with_xy(row) if row[1] == '1'
+      end
+    when 'Refuge Node'
+      CSV.foreach(@file_path) do |row|
+        raw_lines << get_nodes_with_xy(row) if row[1] == '2'
+      end
+    when 'Center Node'
+      CSV.foreach(@file_path) do |row|
+        raw_lines << get_nodes_with_xy(row) if row[1] == '3'
       end
     end
     raw_lines
+  end
+
+  def get_nodes_with_xy(row)
+    [row[0].to_i, row[2].to_f, row[3].to_f]
   end
 end
