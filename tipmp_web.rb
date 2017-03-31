cbc94371b9830e73b911ae54ffbd0ac3fbaf4f14
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-03-31 09:47:22 +0800

    combining two query modes

--- tipmp_web.rb
+++ tipmp_web.rb
@@ -45,7 +45,6 @@ post '/SkylinePathResult' do
     z = false
   end
 
-  center_node = params['source'].to_i
   rain = params['rain'].to_i
 
   dim_multiple = [
@@ -57,22 +56,41 @@ post '/SkylinePathResult' do
   case params['data_set']
   when 'salu'
     mag = MultiAttributeGraph.new(@SALU_NODES, @SALU_EDGES, rain, z, dim_multiple)
-    src_id = if @salu_center_nodes[center_node]
-               @salu_center_nodes[center_node]
-             else
-               center_node
-             end
+    case params['query']
+    when 'center'
+      center_node = params['center'].to_i
+      src_id = if @salu_center_nodes[center_node]
+                @salu_center_nodes[center_node]
+              else
+                center_node
+              end
+      ns = NearestStronghold.new(mag)
+      refuge_node, dst_id = ns.query(src_id, params['data_set'])
+      @title = "#{params['center']} 到 #{refuge_node}計算結果 - (節點#{src_id} to #{dst_id})"
+    when 'srcDest'
+      src_id = params['source'].to_i
+      dst_id = params['dest'].to_i
+      @title = "節點#{src_id} to #{dst_id}計算結果"
+    end
   when 'longjing'
     mag = MultiAttributeGraph.new(@LONGJING_NODES, @LONGJING_EDGES, rain, z, dim_multiple)
-    src_id = if @longjing_center_nodes[center_node]
-               @longjing_center_nodes[center_node]
-             else
-               center_node
-             end
+    case params['query']
+    when 'center'
+      center_node = params['center'].to_i
+      src_id = if @longjing_center_nodes[center_node]
+                @longjing_center_nodes[center_node]
+              else
+                center_node
+              end
+      ns = NearestStronghold.new(mag)
+      refuge_node, dst_id = ns.query(src_id, params['data_set'])
+      @title = "#{params['center']} 到 #{refuge_node}計算結果 - (節點#{src_id} to #{dst_id})"
+    when 'srcDest'
+      src_id = params['source'].to_i
+      dst_id = params['dest'].to_i
+      @title = "節點#{src_id} to #{dst_id}計算結果"
+    end
   end
-
-  ns = NearestStronghold.new(mag)
-  refuge_node, dst_id = ns.query(src_id, params['data_set'])
 
   sp = SkyPath.new(mag)
   query_data = { 'src_id' => src_id, 'dst_id' => dst_id, 'limit' => 1.3, 'evaluate' => 1 }
@@ -85,7 +103,6 @@ post '/SkylinePathResult' do
   )
 
   Writer.output_to_txt(@result, sp, src_id, dst_id)
-  @title = "#{params['source']} 到 #{refuge_node}計算結果 - (節點#{src_id} to #{dst_id})"
 
   @filename_5   = "top_5_#{src_id}to#{dst_id}_result.txt"
   @filename_sum = "sum_best_#{src_id}to#{dst_id}_result.txt"
@@ -100,12 +117,15 @@ end
 ## Functions ##
 def get_params(params)
   {
-    :data => params['data_set'],
-    :src  => params['source'],
-    :rain => params['rain'],
-    :dim1 => params['dim_input_1'],
-    :dim2 => params['dim_input_2'],
-    :dim3 => params['dim_input_3'],
-    :z    => params['z_radio']
+    :data   => params['data_set'],
+    :query  => params['query'],
+    :center => params['center'],
+    :src    => params['source'],
+    :dst    => params['dest'],
+    :rain   => params['rain'],
+    :dim1   => params['dim_input_1'],
+    :dim2   => params['dim_input_2'],
+    :dim3   => params['dim_input_3'],
+    :z      => params['z_radio']
   }
 end
